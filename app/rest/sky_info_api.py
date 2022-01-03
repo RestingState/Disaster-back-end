392f8fa5d93b704a09f3c35a2ff6c62df3098ab3
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-01-03 17:50:30 +0200

    improved weather get method(optional city)

--- app/rest/sky_info_api.py
+++ app/rest/sky_info_api.py
@@ -15,6 +15,7 @@ def get_stars_filter_by_limit():
     UserSchema should be replaces with Star schema
 
     ?limit=Value: int, Default=100
+
     Returns list of stars limited by the given integer
 
     Notice: if limit is greater than actual number of stars it will return all of them
@@ -46,6 +47,7 @@ def get_satellites_filter_by_limit():
     UserSchema should be replaces with Satellite schema
 
     ?limit=Value: int, Default=100
+
     Returns list of satellites limited by the given integer
 
     Notice: if limit is greater than actual number of satellite it will return all of them
@@ -75,14 +77,22 @@ def get_weather_for_user():
     Before using this method make sure the api key in
     app.rest.__init__ is provided for Weather class
 
-    Returns weather for current user location.
-    In case there is a trouble with api you will get 401
+    ?city=Value: str, Default=user`s location
+
+    Returns current weather for the city.
+    In case there is a trouble with city you will get 401
+
+    Notice: all the api errors including api key errors return the same
+    message and code as the city error
     """
 
     session = Session()
     current_identity_username = get_jwt_identity()
 
     user = session.query(User).filter_by(username=current_identity_username).first()
-    city = session.query(City).filter_by(id=user.city_id).first()
-    data = weather.in_the_city(city.name)
-    return (jsonify(data), 200) if data else ({'message': 'Wrong city or api key provided'}, 401)
+    city_object = session.query(City).filter_by(id=user.city_id).first()
+
+    city = request.args.get('city', city_object.name)
+
+    data = weather.in_the_city(city)
+    return (jsonify(data), 200) if data else ({'message': 'Wrong city provided'}, 401)
