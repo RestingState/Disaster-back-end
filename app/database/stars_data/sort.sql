de2f9589093df2a47fef8354568d0b364bb0a7e2
yuliamarkiv <yulia.markiv.27@gmail.com>
2022-01-24 12:37:58 +0200

    structure stars data and add script for sorting stars

---
+++ app/database/stars_data/sort.sql
@@ -0,0 +1,16 @@
+update stars
+set parallax = 0
+where parallax = 'NULL';
+
+-- drop view stars_flux_v
+CREATE VIEW  stars_flux_v AS
+select * from stars
+order by flux_visible_light::decimal DESC;
+
+-- drop view stars_parallax
+CREATE VIEW  stars_parallax AS
+select * from stars
+order by parallax::decimal DESC;
+
+ -- select * from stars_flux_v;
+-- select * from stars_parallax;
