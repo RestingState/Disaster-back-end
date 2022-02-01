d525c362e0c15b579a49fb621ba9ae50ecaa4dde
yuliamarkiv <yulia.markiv.27@gmail.com>
2022-02-01 13:36:57 +0200

    added job for deleting data

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
+import datetime
 from app import api
+from app.rest import *
 from wsgiref.simple_server import make_server
 from app.rest.api import api_blueprint
 from app.rest.sky_info_api import sky_blueprint
-
-
+from apscheduler.schedulers.background import BackgroundScheduler
 api.register_blueprint(api_blueprint)
 api.register_blueprint(sky_blueprint)
+session = Session()
 
-with make_server('', 5000, api) as server:
-    server.serve_forever()
 
-# if __name__ == "__main__":
-#     # api.run('0.0.0.0')
-#     # serve(api, port=5000)
-#     api.run(debug=True, port=5000)
+def delete_records():
+    date_to_delete = datetime.datetime.today() - datetime.timedelta(days=1)
+    records = session.query(PlanetCoordinates).filter(PlanetCoordinates.date <= date_to_delete).all()
+    for i in records:
+        session.delete(i)
+    session.commit()
+
+
+# with make_server('', 5000, api) as server:
+#     server.serve_forever()
+
+
+if __name__ == "__main__":
+    # api.run('0.0.0.0')
+    # serve(api, port=5000)
+    try:
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(func=delete_records, trigger="interval", seconds = 5)
+        # scheduler.add_job(func=delete_records, trigger="cron", day='1st tue', hour='13-14')
+        scheduler.start()
+    except AttributeError:
+        print('There is no data in database')
+
+    api.run(debug=True, port=5000)
