5bfb3c45a29e35462bafe43d1f3729f9759ebe0a
olratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2022-02-02 11:55:48 +0200

    add admin password

--- config.py
+++ config.py
@@ -15,3 +15,4 @@ class Config:
     DB_URI = os.getenv('DB_URI', f'postgresql+psycopg2://{user}:{password}@{server}/{database}')
     JWT_SECRET_KEY = 'super-secret'
     WEATHER_API_KEY = '38a998fffca4ade707ae1065880997a3'
+    ADMIN_PASSWORD = '8Gaw$S@6Paq%rRC'
