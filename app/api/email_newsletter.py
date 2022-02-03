88d0fd7056c589aefac31d1f47227a941b1d35a4
olratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2022-02-03 14:56:33 +0200

    add email validation function and change file name

--- app/api/email_newsletter.py
+++ app/api/email_newsletter.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.message import EmailMessage
 from app.rest import Config
+import requests
 
 
 def send_email(reciever, subject, text):
@@ -21,3 +22,21 @@ def send_email(reciever, subject, text):
     server.login('stellarly.events@gmail.com', Config.ADMIN_PASSWORD)
     server.send_message(msg)
     server.quit()
+
+
+def check_email_existance(email):
+    """
+    email: str
+
+    Uses isitrealemail api to check whether given email exists and returns str status
+
+    If exists, status is 'valid'
+    """
+    api_key = '88eccb50-0c86-48e9-92b3-d6339958e18c'
+    response = requests.get(
+        "https://isitarealemail.com/api/email/validate",
+        params={'email': email},
+        headers={'Authorization': "Bearer " + api_key})
+
+    status = response.json()['status']
+    return status
