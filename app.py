34904512e6eaa04b271d43dd03c30dbb14dd3a6c
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-02-04 14:50:43 +0200

    move insert and delete to different file

--- app.py
+++ app.py
@@ -1,43 +1,13 @@
-import datetime
 from app import api
-from app.rest import *
 from wsgiref.simple_server import make_server
 from app.rest.api import api_blueprint
 from app.rest.sky_info_api import sky_blueprint
 from apscheduler.schedulers.background import BackgroundScheduler
-from app.api.planet import load_planet_coordinates
-from app.api.planet import Planet as PlanetClass
+from app.api.job_functions import delete_records, insert_records
 
 
 api.register_blueprint(api_blueprint)
 api.register_blueprint(sky_blueprint)
-session = Session()
-
-
-def delete_records():
-    date_to_delete = datetime.datetime.today() - datetime.timedelta(days=1)
-    records = session.query(PlanetCoordinates).filter(PlanetCoordinates.date <= date_to_delete).all()
-    for i in records:
-        session.delete(i)
-    session.commit()
-
-
-def insert_records():
-    planets = session.query(Planet).all()
-
-    start_time = datetime.datetime.today()
-    stop_time = datetime.datetime.today() + datetime.timedelta(days=32)
-
-    for planet in planets:
-        try:
-            coordinates = PlanetClass.get_dec_and_ra_in_time_interval(planet.name, start_time, stop_time)
-        except Exception:
-            print('Internal server error')
-        else:
-            loading_result = load_planet_coordinates(planet, coordinates, session)
-            if loading_result:
-                print('Internal server error')
-    session.commit()
 
 
 with make_server('', 5000, api) as server:
@@ -47,9 +17,9 @@ with make_server('', 5000, api) as server:
     try:
         scheduler = BackgroundScheduler()
         scheduler.add_job(func=insert_records, trigger="cron",
-                          year='*', month='*', day=1, hour=14, minute=44, second=0)
+                          year='*', month='*', day=4, hour=14, minute=49, second=0)
         scheduler.add_job(func=delete_records, trigger="cron",
-                          year='*', month='*', day=1, hour=14, minute=45, second=0)
+                          year='*', month='*', day=4, hour=14, minute=50, second=0)
         scheduler.start()
     except AttributeError:
         print('There is no data in database')
---
+++ app/api/job_functions.py
@@ -0,0 +1,35 @@
+from app import Session
+from app.api.planet import load_planet_coordinates
+from app.api.planet import Planet as PlanetClass
+import datetime
+from app.models.models import Planet, PlanetCoordinates
+
+
+def delete_records():
+    session = Session()
+    date_to_delete = datetime.datetime.today() - datetime.timedelta(days=1)
+    records = session.query(PlanetCoordinates).filter(PlanetCoordinates.date <= date_to_delete).all()
+    for i in records:
+        session.delete(i)
+    session.commit()
+    session.close()
+
+
+def insert_records():
+    session = Session()
+    planets = session.query(Planet).all()
+
+    start_time = datetime.datetime.today()
+    stop_time = datetime.datetime.today() + datetime.timedelta(days=32)
+
+    for planet in planets:
+        try:
+            coordinates = PlanetClass.get_dec_and_ra_in_time_interval(planet.name, start_time, stop_time)
+        except Exception:
+            print('Internal server error')
+        else:
+            loading_result = load_planet_coordinates(planet, coordinates, session)
+            if loading_result:
+                print('Internal server error')
+    session.commit()
+    session.close()
