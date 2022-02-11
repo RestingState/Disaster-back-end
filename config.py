a2b3e7ad6b544baccd75973e0daf6b4f59426f07
yuliamarkiv <yulia.markiv.27@gmail.com>
2022-02-11 13:39:00 +0200

    undo changes

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     Provides configuration settings and api keys.
     """
 
-    DATABASE_URL = os.getenv('DATABASE_URL', f'postgresql+psycopg2://tecmint:rolny34F@localhost/test')
+    DB_URI = os.getenv('DB_URI', f'postgresql+psycopg2://tecmint:rolny34F@localhost/test')
     JWT_SECRET_KEY = 'super-secret'
     WEATHER_API_KEY = '38a998fffca4ade707ae1065880997a3'
     ADMIN_PASSWORD = '8Gaw$S@6Paq%rRC'
