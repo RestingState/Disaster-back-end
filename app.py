b3f1efc677607fc5aaf91a48b0d5659f5f52b0f3
yuliamarkiv <yulia.markiv.27@gmail.com>
2022-02-11 11:54:27 +0200

    Add some changes

--- app.py
+++ app.py
@@ -10,16 +10,34 @@ api.register_blueprint(api_blueprint)
 api.register_blueprint(sky_blueprint)
 
 
-with make_server('', 5000, api) as server:
+# with make_server('', 5000, api) as server:
+#
+#     # scheduler
+#     # updates planet_coordinates every month at 1st day and concrete hour(example: 14:44:00)
+#     # deletes old planet_coordinates every month at 1st day and concrete hour(example: 14:45:00)
+#
+#     # dates warning
+#     # in case you change the day of month for jobs you should change update day in
+#     # insert_records function in api/job_functions.py file
+#
+#     try:
+#         scheduler = BackgroundScheduler()
+#         scheduler.add_job(func=insert_records, trigger="cron",
+#                           year='*', month='*', day=1, hour=14, minute=44, second=0)
+#         scheduler.add_job(func=delete_records, trigger="cron",
+#                           year='*', month='*', day=1, hour=14, minute=45, second=0)
+#         scheduler.start()
+#     except AttributeError:
+#         print('There is no data in database')
+#     except Exception:
+#         print('Internal server error')
+#
+#     server.serve_forever()
 
-    # scheduler
-    # updates planet_coordinates every month at 1st day and concrete hour(example: 14:44:00)
-    # deletes old planet_coordinates every month at 1st day and concrete hour(example: 14:45:00)
 
-    # dates warning
-    # in case you change the day of month for jobs you should change update day in
-    # insert_records function in api/job_functions.py file
-
+if __name__ == "__main__":
+    api.run('0.0.0.0')
+   # serve(api, port=5000)
     try:
         scheduler = BackgroundScheduler()
         scheduler.add_job(func=insert_records, trigger="cron",
@@ -32,24 +50,6 @@ with make_server('', 5000, api) as server:
     except Exception:
         print('Internal server error')
 
-    server.serve_forever()
+    api.run(port=5000)
+    # app.run(host='0.0.0.0')
 
-
-# if __name__ == "__main__":
-#     # api.run('0.0.0.0')
-#     # serve(api, port=5000)
-#     try:
-#         scheduler = BackgroundScheduler()
-#         # scheduler.add_job(func=delete_records, trigger="interval", seconds=60)
-#         # scheduler.add_job(func=delete_records, trigger="cron", day='1st tue')
-#         # scheduler.add_job(func=insert_records, trigger="interval", seconds=30)
-#         # scheduler.add_job(func=insert_records, trigger="cron", second='*/10')
-#         scheduler.add_job(func=insert_records, trigger="cron",
-#                           year='*', month='*', day=1, hour=14, minute=39, second=0)
-#         scheduler.add_job(func=delete_records, trigger="cron",
-#                           year='*', month='*', day=1, hour=14, minute=39, second=30)
-#         scheduler.start()
-#     except AttributeError:
-#         print('There is no data in database')
-#
-#     api.run(debug=False, port=5000)
