8ef71bfc2683eec6f465658141f8501ad40bd630
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-02-13 12:06:20 +0200

    fix config

--- config.py
+++ config.py
@@ -12,10 +12,8 @@ class Config:
     Provides configuration settings and api keys.
     """
 
-    # DB_URI = os.getenv('DB_URI', f'postgresql+psycopg2://{user}:{password}@{server}/{database}')
-    DB_URI = os.getenv('DB_URI', f'postgresql://lwkfonojrimaki:b436bfc22e113b3f8b014e65647db47d4d22552157'
-                                 f'6ec1577e36d8a002625c13@ec2-52-215-225-178.eu-west-1.compute.amazonaws.com'
-                                 f':5432/dhvqkklauljc4')
+    DB_URI = os.getenv('DB_URI', f'postgresql://lwkfonojrimaki:b436bfc22e113b3f8b014e65647db47d4d225521576ec1577e36d8a002625c13@ec2-52-215-225-178.eu-west-1.compute.amazonaws.com:5432/dhvqkklauljc4')
     JWT_SECRET_KEY = 'super-secret'
     WEATHER_API_KEY = '38a998fffca4ade707ae1065880997a3'
     ADMIN_PASSWORD = '8Gaw$S@6Paq%rRC'
+
