e45d147114068aa3fb8828993392f2f69c459249
olehratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2022-02-22 09:52:50 +0200

    add stars data

--- app/database/stars_data/stars_data_astroquery.py
+++ app/database/stars_data/stars_data_astroquery.py
@@ -1,22 +1,29 @@
+import time
+import astropy.coordinates as coord
 from astroquery.simbad import Simbad
 from app import Session
-from app.models.models import Star
+from app.models.models import Stars
+import astropy.units as u
 
 
 def update_stars_info(ra: str, dec: str, radius: str):
     session = Session()
 
     Simbad.remove_votable_fields('coordinates')
-    Simbad.add_votable_fields('ra', 'dec', 'flux(V)', 'plx', 'sp')
-    result_table1 = Simbad.query_criteria(f'region(box, GAL, {ra} {dec}, {radius})', otype='*')
-
+    Simbad.add_votable_fields('ra', 'dec', 'flux(V)', 'plx', 'sp', 'otype')
+    result_table1 = Simbad.query_region(coord.SkyCoord(f"{ra} {dec}", frame='icrs'), radius=radius)
     if not result_table1:
+        print('no data')
         return
-
     for record in result_table1:
-        star_data = Star()
+        if record[6] == 'Star' or str(record[6]).find('*') != -1:
+            pass
+        else:
+            continue
 
-        if Session().query(Star).filter_by(name=record[0]).first():
+        star_data = Stars()
+
+        if Session().query(Stars).filter_by(name=record[0]).first():
             continue
 
         if str(record[0]) == '' or str(record[0]) == '--':
@@ -55,14 +62,36 @@ def update_stars_info(ra: str, dec: str, radius: str):
     session.close()
 
 
-ra = 0
-dec = -90
-radius = '1d 1d'
+ra = 'h0m0s'
+radius = '0.5d'
+minus = True
 
-for i in range(1, 360):
-    for j in range(1, 181):
-        update_stars_info(str(ra), str(dec), radius)
-        print(ra, dec)
-        dec = j - 90
-    dec = -90
-    ra = i
+for i in range(0, 24):
+    rightasc = str(i) + ra
+    hours = -6
+    minutes = 0
+    dec = f'{hours}h{minutes}m0s'
+    update_stars_info(rightasc, str(dec), radius)
+    print(rightasc, dec)
+    minus = True
+    for j in range(0, 180):
+        if hours < 0 or minus:
+            if minutes - 4 < 0:
+                minutes = 60 + minutes - 4
+                hours += 1
+            else:
+                if hours == 0 and minutes - 4 == 0:
+                    minus = False
+                minutes -= 4
+        else:
+            if minutes + 4 >= 60:
+                minutes = minutes + 4 - 60
+                hours += 1
+            else:
+                minutes += 4
+        if minus and hours == 0:
+            dec = f'-{hours}h{minutes}m0s'
+        else:
+            dec = f'{hours}h{minutes}m0s'
+        update_stars_info(rightasc, str(dec), radius)
+        print(rightasc, dec)
